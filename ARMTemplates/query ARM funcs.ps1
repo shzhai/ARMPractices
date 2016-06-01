dc5c33253b677ae49c582ecabed2ed2156a36d4d
shzhai <shawnzhai@msn.com>
2016-06-01 21:05:02 +0800

    add method to query supported ARM funcs

---
+++ ARMTemplates/query ARM funcs.ps1
@@ -0,0 +1,2 @@
+﻿$schema = (Invoke-WebRequest "http://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json").content | ConvertFrom-Json
+$schema.definitions.expression.description.Split(',') 
